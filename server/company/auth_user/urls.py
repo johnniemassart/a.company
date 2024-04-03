0e8e9f42367c11d78185cb6e09cfdf735dbc67d7
Johnnie Massart <johnniemassart@Johnnies-MacBook-Pro.local>
2024-04-02 21:25:51 -0500

    server logic

---
+++ server/company/auth_user/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include
+from .views import *
+from rest_framework.routers import DefaultRouter
+
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
+
+router = DefaultRouter()
+router.register("users", UserViewSet)
+
+urlpatterns = [
+    path("", include(router.urls)),
+    path("api/token/", MyTokenObtainView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+]
